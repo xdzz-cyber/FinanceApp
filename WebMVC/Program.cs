18c39ab0c303620775dfd1f5b7d065b1ce2c477f
xdzz-cyber <bogdankrut12@gmail.com>
2023-05-27 16:00:51 +0300

    add ability of adding transactions and showing them for specific budget

--- WebMVC/Program.cs
+++ WebMVC/Program.cs
@@ -63,8 +63,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    // pattern: "{controller=Home}/{action=Index}/{id?}");
-    pattern: "{controller=Auth}/{action=Login}/{id?}");
-// app.MapControllers();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
